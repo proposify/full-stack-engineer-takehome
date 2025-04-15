ea09823b0627f0caf8ed466d10a4178c84a64a06
Yushan <shan.chang@proposify.com>
2025-04-15 15:38:55 -0700

    create components

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
-
+import Editor from "./Editor";
+import LoginBox from "./LoginBox";
 export default function App() {
-
   return (
     <Container maxWidth="md">
       <Box my={4}>
+        <LoginBox />
+        <Editor />
       </Box>
     </Container>
   );
